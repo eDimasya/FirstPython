4a44d28aa683ad0f23a899204e7b97af86581a24
Dima Egorov <edimasya@gmail.com>
2020-04-21 23:46:50 +0700

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,30 @@
+x = "Hello World" #str
+print(type(x))
+x = 1 + 2 + 3
+print(type(x))
+a = 5
+b = 2
+tmp = a
+a = b
+b = tmp
+print(a, b, tmp)
+x = y = z = 0 #каскадное присваивание
+print(x, y, z)
+# множественное присваивание
+x, y, z = 1, 2, 3
+print(x, y, z)
+a, b = 1, 2
+a, b = b, a
+print(a, b)
+x = 5
+y = 3
+z = 2
+print(x ** y ** z)
+print(2/3)
+x = 5
+y = 2
+print(x // y) #div
+print(x % y) #mod
+
+
+
